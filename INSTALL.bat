d4af4eaa940983c5bbf578ffb06b009865d7f75f
shh3482 <shh3482@gmail.com>
2026-03-16 16:21:04 +0900

    폴더화

--- INSTALL.bat
+++ INSTALL.bat
@@ -4,20 +4,28 @@ SET POSH_PROFILE=Microsoft.PowerShell_profile.ps1
 SET POSH_NAME=powershell.ps1
 SET IM_SELECT=im-select.exe
 
-
+REM posh
 DEL "%USERPROFILE%\Documents\PowerShell\%POSH_PROFILE%" 
 DEL "%USERPROFILE%\Documents\WindowsPowerShell\%POSH_PROFILE%" 
-MKLINK "%USERPROFILE%\Documents\PowerShell\%POSH_PROFILE%" "%~dp0\%POSH_NAME%"
-MKLINK "%USERPROFILE%\Documents\WindowsPowerShell\%POSH_PROFILE%" "%~dp0\%POSH_NAME%"
+MKLINK "%USERPROFILE%\Documents\PowerShell\%POSH_PROFILE%" "%~dp0\powershell\%POSH_NAME%"
+MKLINK "%USERPROFILE%\Documents\WindowsPowerShell\%POSH_PROFILE%" "%~dp0\powershell\%POSH_NAME%"
 
+REM vim
 IF NOT EXIST "%LOCALAPPDATA%\nvim" ( MKDIR "%LOCALAPPDATA%\nvim" )
 DEL "%LOCALAPPDATA%\nvim\init.vim" 
-MKLINK "%LOCALAPPDATA%\nvim\init.vim" "%~dp0\neovim.vim"
+MKLINK "%LOCALAPPDATA%\nvim\init.vim" "%~dp0\neovim\neovim.vim"
+MKLINK "%USERPROFILE%\.ideavimrc" "%~dp0\neovim\neovim.vim"
 
+REM vscode
 DEL "%APPDATA%\Code\User\keybindings.json" 
 DEL "%APPDATA%\Code\User\settings.json" 
-MKLINK "%APPDATA%\Code\User\keybindings.json" "%~dp0\vscode-keybindings.json"
-MKLINK "%APPDATA%\Code\User\settings.json" "%~dp0\vscode-settings.json"
+MKLINK "%APPDATA%\Code\User\keybindings.json" "%~dp0\vscode\vscode-keybindings.json"
+MKLINK "%APPDATA%\Code\User\settings.json" "%~dp0\vscode\vscode-settings.json"
 
-COPY %~dp0\%IM_SELECt% C:\%IM_SELECT%
+COPY %~dp0\vscode\%IM_SELECT% C:\%IM_SELECT%
+
+REM surfingkeys
+DEL "%USERPROFILE%\surfingkeys.js"
+MKLINK "%USERPROFILE%\surfingkeys.js" "%~dp0\surfingkeys\surfingkeys.js"
+
 PAUSE
