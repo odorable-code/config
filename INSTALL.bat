b67e4c31c118c1f1d2170c78bc8bc2c6853a2d26
voidkknd17 <voidkknd17@gmail.com>
2026-04-07 20:31:36 +0900

    packages.config added

--- INSTALL.bat
+++ INSTALL.bat
@@ -18,30 +18,30 @@ MKLINK "%LOCALAPPDATA%\nvim\init.vim" "%~dp0\neovim\neovim.vim"
 MKLINK "%USERPROFILE%\.ideavimrc" "%~dp0\neovim\neovim.vim"
 
 REM vscode
-DEL "%APPDATA%\Code\User\keybindings.json" 
-DEL "%APPDATA%\Code\User\settings.json" 
+MOVE /Y "%APPDATA%\Code\User\keybindings.json" "%APPDATA%\Code\User\keybindings.json.bak"
+MOVE /Y "%APPDATA%\Code\User\settings.json" "%APPDATA%\Code\User\settings.json.bak"
 MKLINK "%APPDATA%\Code\User\keybindings.json" "%~dp0\vscode\vscode-keybindings.json"
 MKLINK "%APPDATA%\Code\User\settings.json" "%~dp0\vscode\vscode-settings.json"
 
 COPY %~dp0\vscode\%IM_SELECT% C:\%IM_SELECT%
 
 REM surfingkeys
-DEL "%USERPROFILE%\surfingkeys.js"
+MOVE /Y "%USERPROFILE%\surfingkeys.js" "%USERPROFILE%\surfingkeys.js.bak"
 MKLINK "%USERPROFILE%\surfingkeys.js" "%~dp0\surfingkeys\surfingkeys.js"
 
 REM git
-DEL "%USERPROFILE%\.gitconfig"
+MOVE /Y "%USERPROFILE%\.gitconfig" "%USERPROFILE%\.gitconfig.bak"
 MKLINK "%USERPROFILE%\.gitconfig" "%~dp0\git\.gitconfig"
 
 REM keypirinha
 FOR %%a in (%~dp0\keypirinha\*.ini) do (
-	DEL "%APPDATA%\Keypirinha\User\%%~nxa"
+	MOVE /Y "%APPDATA%\Keypirinha\User\%%~nxa" "%APPDATA%\Keypirinha\User\%%~nxa.bak"
 	MKLINK "%APPDATA%\Keypirinha\User\%%~nxa" "%%a"
 )
 
 REM autohotkey
 FOR %%h in (%~dp0\autohotkey\*.ahk) do (
-	DEL "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%%~nxh"
+	MOVE /Y "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%%~nxh" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%%~nxh.bak"
 	MKLINK "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\%%~nxh" "%%h"
 )
 PAUSE
